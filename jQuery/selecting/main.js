ed1bf84f5e2e5069a9436558de9d3b5116f0b71e
MilanMixa <stankovicmilan990@gmail.com>
2021-06-19 23:49:47 +0200

    selecting exercise -> finished

---
+++ jQuery/selecting/main.js
@@ -0,0 +1,25 @@
+// <!--Selecting
+// Create a page that contains navigation (ul element)
+// Navigation should contain odd number of elements
+// Select the first element of the list and give it a bottom border
+// Select all list elements and transform their text to uppercase
+// Select active element of the list and change its font color
+// Find the middle one and change its background color-->
+
+$("li:first").css({
+    "border-bottom": "1px solid black",
+})
+
+// $("li").addClass("upper")
+
+$("li").css({
+    "text-transform": "uppercase",
+})
+
+$("li.active").css({
+    "color": "red",
+})
+
+$("li:nth-child(3)").css({
+    "background-color": "grey",
+})
