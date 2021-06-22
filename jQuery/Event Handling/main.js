f78f71e2b2edeb8de83d5de8be0f2c76edda31ba
MilanMixa <stankovicmilan990@gmail.com>
2021-06-22 19:36:53 +0200

    soccer player ->finished(jQuery)

--- jQuery/Event Handling/main.js
+++ jQuery/Event Handling/main.js
@@ -1,3 +1,24 @@
 var $soccerPlayer = $("img");
-var $soccerField = $("#footbalField");
+var $soccerField = $("#footballField");
 var $button = $("#send");
+var isSet = true;
+
+var playerHandler = function(event){
+    var width = (event.offsetX - $soccerPlayer.width() / 2) + "px";
+    var height =  (event.offsetY - $soccerPlayer.height() / 2) + "px";
+    $soccerPlayer.css({
+        left: width,
+        top: height
+    });
+};
+
+var stop = function(){
+    if(isSet){
+        $soccerField.off("click", playerHandler);
+    } else {
+        $soccerField.on("click", playerHandler);
+    }
+    isSet = !isSet;
+};
+$soccerField.on("click", playerHandler);
+$button.on("click", stop);
